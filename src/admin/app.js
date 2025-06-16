21ac51070e892356e86f46787eb268ed3ac8e4fe
Edu <133777880+Emmakex@users.noreply.github.com>
2025-06-16 13:58:02 +0000

    Vale

--- src/admin/app.js
+++ src/admin/app.js
@@ -35,12 +35,12 @@ const bootstrap = (app) => {
 
 export default {
   config: {
-    locales: ["en","es"],
+    
     translations: {
-      en: {
+      es: {
         "Auth.form.button.forgot-password": "Send Email",
       },
-      es: {
+      en: {
         "Analytics": "Analytics",
   "Auth.components.Oops.text": "Tu cuenta ha sido suspendida",
   "Auth.components.Oops.text.admin": "Si se trata de un error, comuníquese con su administrador.",
